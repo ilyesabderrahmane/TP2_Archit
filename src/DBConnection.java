5b6864a91169894bf5e15538e31ca67680cabb01
ilyesabderrahmane <lyessou2001@gmail.com>
2022-12-03 15:24:28 +0100

    patron singleton qst2

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -2,25 +2,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
-	   
-		String BDD = "nomBD";
-		String url = "jdbc:mysql://localhost:3306/" + BDD;
-		String user = "root";
-		String passwd = "";
-	    private Connection conn;
+public class DBConnection{
+	String BDD = "nomBD";
+	String url = "jdbc:mysql://localhost:3306/" + BDD;
+	String user = "root";
+	String passwd = "";
+    private Connection conn;
+    private static DBConnection instance;
 
-	   
-	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
-		}
 
-	    
-	    public Connection getConn() {
+    private DBConnection() throws SQLException {
+		conn = DriverManager.getConnection(url, user,passwd);}
+		public Connection getConn() {
 			return conn;
 		}
 
-
-		
-	
+	    public static DBConnection getInstance() throws SQLException {
+	    	if(instance == null) {
+	    		instance = new DBConnection();
+	    	}
+	    	return instance;
+	    }
 }
